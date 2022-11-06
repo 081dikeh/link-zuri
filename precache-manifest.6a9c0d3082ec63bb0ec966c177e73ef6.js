b72ad370c717746610432e1c539774eb1fcd659e
Dikeh Daniel <dikehdaniel2020@gmail.com>
2022-11-05 21:08:18 -0700

    Updates

---
+++ precache-manifest.6a9c0d3082ec63bb0ec966c177e73ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c14ddaa96b3fec0cdd33",
+    "url": "/link-zuri/static/css/main.171b2923.chunk.css"
+  },
+  {
+    "revision": "c14ddaa96b3fec0cdd33",
+    "url": "/link-zuri/static/js/main.c14ddaa9.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/link-zuri/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "c62afc3411a6f7e328a0",
+    "url": "/link-zuri/static/js/2.c62afc34.chunk.js"
+  },
+  {
+    "revision": "27235206568264536f36",
+    "url": "/link-zuri/static/js/runtime~main.27235206.js"
+  },
+  {
+    "revision": "eb51ce6ded67cadc4493d59df7396cea",
+    "url": "/link-zuri/static/media/profile-image.eb51ce6d.jpg"
+  },
+  {
+    "revision": "4f663b0a3a1d482684fe6308b641fd28",
+    "url": "/link-zuri/index.html"
+  }
+];
